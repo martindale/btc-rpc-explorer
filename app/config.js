849ea00654aef1047eb7f7799070577e5b6b222a
Eric Martindale <eric@ericmartindale.com>
2018-09-25 16:17:16 -0400

    Disable various features

--- app/config.js
+++ app/config.js
@@ -4,8 +4,8 @@ var coins = require("./coins.js");
 var currentCoin = "BTC";
 
 module.exports = {
-	cookiePassword: "0x000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f",
-	demoSite: true,
+	cookiePassword: "0x00000fdd334285b4e9193a559fea692260bad1d281bf",
+	demoSite: false,
 	coin: currentCoin,
 
 	rpcBlacklist:[
@@ -33,8 +33,8 @@ module.exports = {
 	],
 
 	site: {
-		blockTxPageSize:20,
-		blockTxMaxInput:15,
+		blockTxPageSize:100,
+		blockTxMaxInput:25,
 		browseBlocksPageSize:20
 	},
 
@@ -53,25 +53,24 @@ module.exports = {
 		{name:"Mempool Summary", url:"/mempool-summary", desc:"Detailed summary of the current mempool for this node.", fontawesome:"fas fa-clipboard-list"},
 		{name:"Unconfirmed Transactions", url:"/unconfirmed-tx", desc:"Browse unconfirmed/pending transactions.", fontawesome:"fas fa-unlock-alt"},
 
-		{name:"RPC Browser", url:"/rpc-browser", desc:"Browse the RPC functionality of this node. See docs and execute commands.", fontawesome:"fas fa-book"},
-		{name:"RPC Terminal", url:"/rpc-terminal", desc:"Directly execute RPCs against this node.", fontawesome:"fas fa-terminal"},
+		// {name:"RPC Browser", url:"/rpc-browser", desc:"Browse the RPC functionality of this node. See docs and execute commands.", fontawesome:"fas fa-book"},
+		// {name:"RPC Terminal", url:"/rpc-terminal", desc:"Directly execute RPCs against this node.", fontawesome:"fas fa-terminal"},
 		
 		{name:(coins[currentCoin].name + " Fun"), url:"/fun", desc:"See fun/interesting historical blockchain data.", fontawesome:"fas fa-certificate"}
 	],
 
 	donationAddresses:{
-		coins:["BTC", "LTC"],
+		coins:["BTC"],
 
-		"BTC":{address:"3NPGpNyLLmVKCEcuipBs7G4KpQJoJXjDGe", urlPrefix:"bitcoin:"},
-		"LTC":{address:"ME4pXiXuWfEi1ANBDo9irUJVcZBhsTx14i", urlPrefix:"litecoin:"}
+		"BTC":{address:"3CHGLadfbcKrM1sS5uYtASaq75VAuMSMpb", urlPrefix:"bitcoin:"},
+		// "LTC":{address:"ME4pXiXuWfEi1ANBDo9irUJVcZBhsTx14i", urlPrefix:"litecoin:"}
 	},
 
 	headerDropdownLinks: {
-		title:"Related Sites",
+		title:"Rooms",
 		links:[
-			{name: "Bitcoin Explorer", url:"https://btc.chaintools.io", imgUrl:"/img/logo/btc.svg"},
-			{name: "Litecoin Explorer", url:"https://ltc.chaintools.io", imgUrl:"/img/logo/ltc.svg"},
-			{name: "Lightning Explorer", url:"https://lightning.chaintools.io", imgUrl:"/img/logo/lightning.svg"},
+			{name: "#bitcoin:fabric.pub", url:"https://chat.fabric.pub/#/room/#bitcoin:fabric.pub", imgUrl:"/img/logo/btc.svg"},
+			{name: "#lightning:fabric.pub", url:"https://chat.fabric.pub/#/room/#lightning:fabric.pub", imgUrl:"/img/logo/lightning.svg"},
 		]
 	}
 };
